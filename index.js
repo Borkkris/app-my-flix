88fb2cc52dc4a94f74a8e4db839d1d672f422f31
Borkkris <Borkkris@users.noreply.github.com>
2022-08-23 15:50:56 +0200

    Update index.js
    minor fixes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors({
 //local adress
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); 
 //link (online)
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
