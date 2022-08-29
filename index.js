5545d54b1625ff87c5912f9f60b54461b78382d2
Borkkris <Borkkris@users.noreply.github.com>
2022-08-30 00:09:35 +0200

    Update index.js
    cors

--- index.js
+++ index.js
@@ -23,20 +23,20 @@ const passport = require('passport');
 require('./passport');
 
 // Configure Allowed Domains for Cross-Origin Resource Sharing (CORS)
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
-// app.use(cors()); // CORS Option 1: Allow all domains
+app.use(cors()); // CORS Option 1: Allow all domains
 
-app.use(cors({ // CORS Option 2: Only allow specific domains (see the variable: allowedOrigins)
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1) {// If a specific origin isn’t found on the list of allowed origins
-      let message = `The CORS policy for this application doesn't allow access from origin`+ origin;
-        return callback(new Error(message), false); 
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({ // CORS Option 2: Only allow specific domains (see the variable: allowedOrigins)
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1) {// If a specific origin isn’t found on the list of allowed origins
+//       let message = `The CORS policy for this application doesn't allow access from origin`+ origin;
+//         return callback(new Error(message), false); 
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 //This allows Mongoose to connect to that database (myFlixDB) so it can perform CRUD operations on the documents it contains from within my REST API
 //local adress
