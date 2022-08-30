39b3879d0b7db6d21ea76ae013d326f2adca82e6
Borkkris <Borkkris@users.noreply.github.com>
2022-08-30 13:13:04 +0200

    change back mongoose uri

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ app.use(cors()); // CORS Option 1: Allow all domains
 
 //This allows Mongoose to connect to that database (myFlixDB) so it can perform CRUD operations on the documents it contains from within my REST API
 //local adress
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); 
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); 
 //link (online)
-// mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
